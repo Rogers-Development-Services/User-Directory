ef8e433b3c0980e9bc360b54777ac87b9d2037d9
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-11-16 14:15:59 -0800

    Updates

--- user-directory/src/components/FirstNameSortButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FirstNameSortButton(props) {
-    return <button onClick={props.toggleSortDirection}>First Name</button>
-}
-
-export default FirstNameSortButton;
--- user-directory/src/components/LastNameSortButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LastNameSortButton(props) {
-    return <button onClick={props.toggleSortDirection}>Last Name</button>
-}
-
-export default LastNameSortButton;
